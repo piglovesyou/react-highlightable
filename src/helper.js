1553bbb879ee35941a98cb0dcfe308044c5eef6f
Takamura Soichi <takamura_s@ariel-networks.com>
2016-02-20 09:54:20 +0000

    it works

---
+++ src/helper.js
@@ -0,0 +1,78 @@
+import Rangy from 'rangy';
+
+// Excellent work by Ömür Yanıkoğlu
+// Thanks: http://stackoverflow.com/a/21437499
+module.exports = {
+
+    saveSelection: function(containerEl) {
+        var charIndex = 0, start = 0, end = 0, foundStart = false, stop = {};
+        var sel = Rangy.getSelection(), range;
+
+        function traverseTextNodes(node, range) {
+            if (node.nodeType == 3) {
+                if (!foundStart && node == range.startContainer) {
+                    start = charIndex + range.startOffset;
+                    foundStart = true;
+                }
+                if (foundStart && node == range.endContainer) {
+                    end = charIndex + range.endOffset;
+                    throw stop;
+                }
+                charIndex += node.length;
+            } else {
+                for (var i = 0, len = node.childNodes.length; i < len; ++i) {
+                    traverseTextNodes(node.childNodes[i], range);
+                }
+            }
+        }
+
+        if (sel.rangeCount) {
+            try {
+                traverseTextNodes(containerEl, sel.getRangeAt(0));
+            } catch (ex) {
+                if (ex != stop) {
+                    throw ex;
+                }
+            }
+        }
+
+        return {
+            start: start,
+            end: end
+        };
+    },
+
+    restoreSelection: function(containerEl, savedSel) {
+        var charIndex = 0, range = Rangy.createRange(), foundStart = false, stop = {};
+        range.collapseToPoint(containerEl, 0);
+
+        function traverseTextNodes(node) {
+            if (node.nodeType == 3) {
+                var nextCharIndex = charIndex + node.length;
+                if (!foundStart && savedSel.start >= charIndex && savedSel.start <= nextCharIndex) {
+                    range.setStart(node, savedSel.start - charIndex);
+                    foundStart = true;
+                }
+                if (foundStart && savedSel.end >= charIndex && savedSel.end <= nextCharIndex) {
+                    range.setEnd(node, savedSel.end - charIndex);
+                    throw stop;
+                }
+                charIndex = nextCharIndex;
+            } else {
+                for (var i = 0, len = node.childNodes.length; i < len; ++i) {
+                    traverseTextNodes(node.childNodes[i]);
+                }
+            }
+        }
+
+        try {
+            traverseTextNodes(containerEl);
+        } catch (ex) {
+            if (ex == stop) {
+                Rangy.getSelection().setSingleRange(range);
+            } else {
+                throw ex;
+            }
+        }
+    }
+};
--- src/react-highlightable.js
+++ src/react-highlightable.js
@@ -1,18 +1,41 @@
 import React from 'react';
+import Helper from './helper';
 
 export default class ContentEditable extends React.Component {
   constructor() {
     super();
-    this.emitChange = this.emitChange.bind(this);
+    this.emitChange_ = this.emitChange_.bind(this);
+  }
+
+  componentWillMount() {
+    if (this.props.prefix) {
+      this.prefixRegex_ = new RegExp(`(${this.props.prefix}[^s.]+)`, 'g');
+    } else if (this.props.tokens) {
+      this.tokenRegexTable_ = Object.create(null);
+      this.props.tokens.forEach(token => {
+        this.tokenRegexTable_[token] = new RegExp(`(${token})`, 'g');
+      });
+    } else {
+      throw new Error();
+    }
+    if (this.prefixRegex_ && this.props.tokens.length) {
+      this.replaceFn_ = (_, token, offset) => {
+        return this.tokensTable_[token] ? this.props.highlighter(token, offset) : token;
+      };
+    } else {
+      this.replaceFn_ = (_, token, offset) => {
+        return this.props.highlighter(token, offset) || token;
+      };
+    }
   }
 
   render() {
     return React.createElement(
       this.props.tagName || 'div',
       Object.assign({}, this.props, {
-        ref: (e) => this.htmlEl = e,
-        onInput: this.emitChange,
-        onBlur: this.emitChange,
+        ref: (e) => this.htmlEl_ = e,
+        onInput: this.emitChange_,
+        onBlur: this.emitChange_,
         contentEditable: !this.props.disabled,
         dangerouslySetInnerHTML: {__html: this.props.html}
       }),
@@ -20,23 +43,36 @@ export default class ContentEditable extends React.Component {
   }
 
   shouldComponentUpdate(nextProps) {
-    return !this.htmlEl || nextProps.html !== this.htmlEl.innerHTML ||
-            this.props.disabled !== nextProps.disabled;
+    return nextProps.html !== this.htmlEl_.innerHTML ||
+           this.props.disabled !== nextProps.disabled;
   }
 
   componentDidUpdate() {
-    if ( this.htmlEl && this.props.html !== this.htmlEl.innerHTML ) {
-     this.htmlEl.innerHTML = this.props.html;
+    if ( this.props.html !== this.htmlEl_.innerHTML ) {
+     this.htmlEl_.innerHTML = this.props.html;
     }
   }
 
-  emitChange(evt) {
-    if (!this.htmlEl) return;
-    var html = this.htmlEl.innerHTML;
-    if (this.props.onChange && html !== this.lastHtml) {
-      evt.target = { value: html };
-      this.props.onChange(evt);
+  emitChange_(evt) {
+    const textContent = this.htmlEl_.textContent;
+    if (textContent !== this.lastTextContent_) {
+      const pos = Helper.saveSelection(this.htmlEl_);
+      let formatted;
+      if (this.prefixRegex_) {
+        formatted = textContent.replace(this.prefixRegex_, this.replaceFn_);
+      } else {
+        formatted = textContent;
+        for (let token in this.tokenRegexTable_) {
+          const tokenRegex = this.tokenRegexTable_[token];
+          formatted = formatted.replace(tokenRegex, this.replaceFn_);
+        }
+      }
+      this.htmlEl_.innerHTML = formatted;
+      Helper.restoreSelection(this.htmlEl_, pos);
+      if (this.props.onChange) {
+        this.props.onChange(evt);
+      }
     }
-    this.lastHtml = html;
+    this.lastTextContent_ = textContent;
   }
 }
